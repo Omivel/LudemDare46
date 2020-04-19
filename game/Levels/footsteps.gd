bf4e819e414c5481e5ad2b90dc27e780624132bf
theseldomseen <minormotionpicturemusic@gmail.com>
2020-04-19 15:25:01 -0400

    modified:   game/Levels/footsteps.gd 	modified:   game/Levels/open-door.gd 	modified:   game/Scenes/Trigger/Trigger.gd

--- game/Levels/footsteps.gd
+++ game/Levels/footsteps.gd
@@ -8,8 +8,6 @@ func _ready():
 
 func _on_KinematicBody2D_is_moving(status):
 	if status && (get_volume_db() != 0):
-		print("playing",status)
 		set_volume_db(0)
 	elif !status && (get_volume_db() == 0):
-		print("stopping",status)
 		set_volume_db(-80)
