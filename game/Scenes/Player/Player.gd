fd5b2ead56b95fab1b9b11a6cb340026049c7cea
theseldomseen <minormotionpicturemusic@gmail.com>
2020-04-18 23:21:15 -0400

    game/.DS_Store 	game/.import/close-dry.wav-0594773e3ef98f98b3b8e8655d336c6b.md5 	game/.import/close-dry.wav-0594773e3ef98f98b3b8e8655d336c6b.sample 	game/.import/close-steam.wav-cea975b04d33a026e54ee88a358ec6ae.md5 	game/.import/close-steam.wav-cea975b04d33a026e54ee88a358ec6ae.sample 	game/.import/drone-base.wav-99cb74fc961d816b43b978f4156661c1.md5 	game/.import/drone-base.wav-99cb74fc961d816b43b978f4156661c1.sample 	game/.import/drone-bugs.wav-162730d00905bd993f53b38a3f1fb231.md5 	game/.import/drone-bugs.wav-162730d00905bd993f53b38a3f1fb231.sample 	game/.import/drone-pursuit.wav-aec20edc8807d22ff4f5614bbea8e924.md5 	game/.import/drone-pursuit.wav-aec20edc8807d22ff4f5614bbea8e924.sample 	game/.import/drone-rhodes.wav-7d32deb39504acfac69c56c719356d4f.md5 	game/.import/drone-rhodes.wav-7d32deb39504acfac69c56c719356d4f.sample 	game/.import/end-state.wav-5adb3fd291624e3f5affcc606df3307b.md5 	game/.import/end-state.wav-5adb3fd291624e3f5affcc606df3307b.sample 	game/.import/fail-state.wav-8a6596b36201db2df7f590e4826f9aa0.md5 	game/.import/fail-state.wav-8a6596b36201db2df7f590e4826f9aa0.sample 	game/.import/footsteps.wav-c0d2eafe86f60a8359df6fea4b270182.md5 	game/.import/footsteps.wav-c0d2eafe86f60a8359df6fea4b270182.sample 	game/.import/open-dry.wav-f00b4edd607810b9dd218ba7a217e55d.md5 	game/.import/open-dry.wav-f00b4edd607810b9dd218ba7a217e55d.sample 	game/.import/open-steam.wav-022cda5c379a5fdd208140cfdafa87b6.md5 	game/.import/open-steam.wav-022cda5c379a5fdd208140cfdafa87b6.sample 	game/.import/panel-interact.wav-dac48ad99aef6630a965501846d61c64.md5 	game/.import/panel-interact.wav-dac48ad99aef6630a965501846d61c64.sample 	game/.import/teethy-licks.wav-51069ad28760fea3f31cec3e3b84097d.md5 	game/.import/teethy-licks.wav-51069ad28760fea3f31cec3e3b84097d.sample 	game/Levels/Timer.gd 	game/Levels/main-drone.gd 	game/Levels/pursuit-1.gd 	game/Levels/pursuit-2.gd 	game/Levels/rhodes.gd 	game/default_bus_layout.tres 	game/music/ 	game/sound fx/

--- game/Scenes/Player/Player.gd
+++ game/Scenes/Player/Player.gd
@@ -42,4 +42,5 @@ func _input(event):
 					dist = node.get_global_position().distance_to(get_global_position())
 					if dist < dist_lowest:
 						lowest_node = node
-		lowest_node.activate()
+		if (lowest_node != null):
+			lowest_node.activate()
