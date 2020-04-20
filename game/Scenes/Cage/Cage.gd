e2ce540e293da29470b6ab59d8944597babc1a40
Omivel <maxmilianboyson@gmail.com>
2020-04-19 22:32:15 -0400

    bug fixes

--- game/Scenes/Cage/Cage.gd
+++ game/Scenes/Cage/Cage.gd
@@ -12,13 +12,13 @@ func _ready():
 func _trap_monster(body):
 	if !full:
 		if body is Monster:
-			body.set_physics_process(false)
 			body.set_traped(true)
-			body.collisionShape.set_disabled(true)
-			body.vision.set_monitoring(false)
-			body.vision.set_monitorable(false)
-			body.atack.set_monitoring(false)
-			body.atack.set_monitorable(false)
+			body.set_physics_process(false)
+			body.collisionShape.set_deferred("set_disabled",true)
+			body.vision.set_deferred("set_monitoring",false)
+			body.vision.set_deferred("set_monitorable",false)
+			body.atack.set_deferred("set_monitoring",false)
+			body.atack.set_deferred("set_monitorable",false)
 			body.set_global_position(global_position)
 			full = true
 			emit_signal("traped")
